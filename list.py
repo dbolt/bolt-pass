7965aed8e6a6fbe1661f782c710b7e94f6b1e305
Derek Bolt🐾 <derebolt@amazon.com>
2018-09-30 16:47:34 -0700

    Hello world DDB in ListResources

--- list.py
+++ list.py
@@ -1,7 +1,14 @@
 import json
 import datetime
+import boto3
+
+table = boto3.resource('dynamodb').Table('bolt-pass-resources')
 
 def handler(event, context):
+    print('Event: ' + json.dumps(event))
+    response = table.get_item(Key={ 'id': 'id-1' })
+    print('DynamoDB Response: ' + json.dumps(response))
+
     data = {
         'output': 'Hello World List',
         'timestamp': datetime.datetime.utcnow().isoformat()
